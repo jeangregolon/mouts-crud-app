14c57b1dff107a47e23c0a57e0a28f29238108f1
Jean Gregolon <jean.gregolon@gmail.com>
2025-07-14 14:41:38 -0300

    Fixed dotenv

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm';
 import { User } from './users/user.entity';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
